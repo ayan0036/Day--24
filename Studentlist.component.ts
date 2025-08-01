839a444bba5647c3ec30e04d062903ed1e99fcfc
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-08-01 12:18:53 +0530

    Create Studentlist.component.ts

---
+++ Studentlist.component.ts
@@ -0,0 +1,18 @@
+ import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../services/student.service';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html'
+})
+export class StudentListComponent implements OnInit {
+  students: any[] = [];
+
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit() {
+    this.studentService.getStudents().subscribe(data => {
+      this.students = data;
+    });
+  }
+}
